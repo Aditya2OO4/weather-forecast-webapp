00686ca9d521f541059c3a6e39f8ac00597808c4
Aditya <adityasnalla1412.com>
2024-12-02 22:50:45 +0530

    Filtered Forecast data.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.express as px
 
+from backend import get_data
+
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ",help="Enter city name")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -10,12 +12,9 @@ option = st.selectbox("Select data to view",
 
 st.subheader(f"{option} for next {days} days in {place}")
 
+data = get_data(place, days, option)
 #Graph
-def get_days(days):
-    dates = ["2024-1-11", "2024-2-11", "2024-3-1"]
-    temperature = [10, 11, 15]
-    temperature = [days * i for i in temperature]
-    return dates, temperature
+
 
 d, t = get_days(days)
 figure = px.line(x=d, y=t, labels={"x":"Date", "y":"Temperature (C)"})
